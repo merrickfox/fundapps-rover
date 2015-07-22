ac717b10b98cb154f13d9eb06b653c342e3783e4
merrick fox <merrick.fox@gmail.com>
2015-07-22 21:08:40 +0100

    test for the rovers internal variables and their initial values

--- test.js
+++ test.js
@@ -1,2 +1,15 @@
+describe('Initial settings', function() {
 
+  it('The rover should have a x coordinate and it should be 0 initially', function() {
+    rover.x.should.be.exactly(0).and.be.a.Number;
+  });
+
+  it('The rover should have a y coordinate and it should be 0 initially', function() {
+    rover.y.should.be.exactly(0).and.be.a.Number;
+  });
+
+  it('The rover should have a direction variable and it should be N initially', function() {
+    rover.direction.should.be('N');
+  });
+});
 
